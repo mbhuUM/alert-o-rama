1cf37e4fcbd7cacd48986c4996d4f7168f3d7c19
mbhuUM <mbhu@umich.edu>
2025-02-09 15:32:59 -0500

    begginning of learning how to web scrape

--- main.go
+++ main.go
@@ -1,9 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+	"encoding/csv"
+    "os"
+	"log" 
 
-var bestBuyKey string 
+)
+
+type Product struct {
+    Url, Image, Name, Price string
+}
+
+
 
 func main() {
-	fmt.Print(bestBuyKey)
+
+    c := colly.NewCollector(
+        colly.AllowedDomains("www.scrapingcourse.com"),)
+
+	
+    // called before an HTTP request is triggered
+    c.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting: ", r.URL)
+    })
+
+	var products []Product
+
+	 // OnHTML callback
+	 c.OnHTML("li.product", func(e *colly.HTMLElement) {
+
+        // initialize a new Product instance
+        product := Product{}
+
+        // scrape the target data
+        product.Url = e.ChildAttr("a", "href")
+        product.Image = e.ChildAttr("img", "src")
+        product.Name = e.ChildText(".product-name")
+        product.Price = e.ChildText(".price")
+
+        // add the product instance with scraped data to the list of products
+        products = append(products, product)
+    })
+
+	c.OnScraped(func(r *colly.Response) {
+
+        // open the CSV file
+        file, err := os.Create("products.csv")
+        if err != nil {
+            log.Fatalln("Failed to create output CSV file", err)
+        }
+        defer file.Close()
+
+        // initialize a file writer
+        writer := csv.NewWriter(file)
+
+        // write the CSV headers
+        headers := []string{
+            "Url",
+            "Image",
+            "Name",
+            "Price",
+        }
+        writer.Write(headers)
+
+        // write each product as a CSV row
+        for _, product := range products {
+            // convert a Product to an array of strings
+            record := []string{
+                product.Url,
+                product.Image,
+                product.Name,
+                product.Price,
+            }
+
+            // add a CSV record to the output file
+            writer.Write(record)
+        }
+        defer writer.Flush()
+    })
+	
+	// open the target URL
+	c.Visit("https://www.scrapingcourse.com/ecommerce")
+
 }
